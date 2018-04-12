eb79bc7b3fdc2421cfe3ab7f79fb8b03af2fa920
王涛 <dadtakesmefly@gmail.com>
2018-04-12 16:21:20 +0800

    Update common.js

--- common.js
+++ common.js
@@ -516,4 +516,30 @@ function GetQueryString(name) {
       })
     }
   },
+      
+//js计算数组中元素出现的次数，并实现去重
+function getCount(arr, rank, ranktype) {
+  var obj = {}, k, arr1 = [];
+  for (var i = 0, len = arr.length; i < len; i++) {
+    k = arr[i];
+    if (obj[k])
+      obj[k]++;
+    else
+      obj[k] = 1;
+  }
+  //保存结果{el-'元素'，count-出现次数}
+  for (var o in obj) {
+    arr1.push({ el: o, count: obj[o] });
+  }
+  //排序（降序）
+  arr1.sort(function (n1, n2) {
+    return n2.count - n1.count
+  });
+  //如果ranktype为1，则为升序，反转数组
+  if (ranktype === 1) {
+    arr1 = arr1.reverse();
+  }
+  var rank1 = rank || arr1.length;
+  return arr1.slice(0, rank1);
+}
 
