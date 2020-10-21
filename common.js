2af747a00408b711ad2a9ecdd1c6195c657eaadc
王涛 <dadtakesmefly@gmail.com>
2020-10-21 14:52:59 +0800

    Update common.js

--- common.js
+++ common.js
@@ -3,6 +3,37 @@
  */
 
 
+/**
+ * @param {string} format
+ * @param {number} timestamp - 时间戳
+ * @return {string} 
+ */
+function formatDate(format='Y-M-D h:m', timestamp=Date.now()){
+  let date = new Date(timestamp)
+  let dateInfo = {
+    Y: date.getFullYear(),
+    M: date.getMonth()+1,
+    D: date.getDate(),
+    h: date.getHours(),
+    m: date.getMinutes(),
+    s: date.getSeconds()
+  }
+  let formatNumber = (n) => n > 10 ? n : '0' + n
+  let res = format
+    .replace('Y', dateInfo.Y)
+    .replace('M', dateInfo.M)
+    .replace('D', dateInfo.D)
+    .replace('h', formatNumber(dateInfo.h))
+    .replace('m', formatNumber(dateInfo.m))
+    .replace('s', formatNumber(dateInfo.s))
+  return res
+}
+
+formatDate() // "2020-2-24 13:44"
+formatDate('M月D日 h:m') // "2月24日 13:45"
+formatDate('h:m Y-M-D', 1582526221604) // "14:37 2020-2-24"
+
+
 // 关于JS实现的4种数字千位符格式化，例如“10,000”
 function toThousands(num) {
     return (num || 0).toString().replace(/(\d)(?=(?:\d{3})+$)/g, '$1,');
